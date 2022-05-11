d7f23419364919adba7f11a81a95ae601135a9d8
Thiago <thiagocdn@gmail.com>
2022-05-11 20:57:58 -0300

    Added checkout backend layer

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,12 +1,10 @@
-import { FaGithub } from 'react-icons/fa'
-import { signIn, signOut, useSession } from 'next-auth/react'
-import { FiX } from 'react-icons/fi'
-import styles from './styles.module.scss'
+import { FaGithub } from "react-icons/fa";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const { data } = useSession();
-
-  console.log('data', data)
 
   return data?.user ? (
     <button
@@ -14,18 +12,18 @@ export function SignInButton() {
       className={styles.signInButton}
       onClick={() => signOut()}
     >
-      <FaGithub color='#04d361' />
-       {data.user.name}
-      <FiX color='#737380' className={styles.closeIcon} />
+      <FaGithub color="#04d361" />
+      {data.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => signIn('github')}
+      onClick={() => signIn("github")}
     >
-      <FaGithub color='#eba517' />
+      <FaGithub color="#eba517" />
       Sign in with Github
     </button>
-  )
+  );
 }
--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +6,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data } = useSession();
+  
+  function handleSubscribe() {
+    if(!data.user) {
+      signIn('github')
+      return;
+    }
+
+    
+  }
+
   return (
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
