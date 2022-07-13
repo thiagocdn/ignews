eca4f40aa2a5395622c1db9994094155c4130ba4
Thiago <thiagocdn@gmail.com>
2022-07-12 21:43:23 -0300

    Double call on getServerSideProps

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
-import { query as q } from 'faunadb'
+import { query as q } from "faunadb";
 
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,44 +13,58 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user'
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
   ],
   callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), q.Casefold("active")),
+            ])
+          )
+        );
+
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn({ user, account, profile }) {
-      const { email } = user
+      const { email } = user;
 
       try {
         await fauna.query(
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email }}
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
-        )
-  
-        return true 
+        );
+        return true;
+      } catch {
+        return false;
       }
-      catch {
-        return false
-      }
-    }
-  }
-})
+    },
+  },
+});
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,9 +1,9 @@
-import { GetServerSideProps } from "next"
-import { getSession } from "next-auth/react"
-import Head from "next/head"
-import { RichText } from "prismic-dom"
-import { getPrismicClient } from "../../services/prismic"
-import styles from './post.module.scss'
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+import styles from "./post.module.scss";
 
 interface PostProps {
   post: {
@@ -11,11 +11,10 @@ interface PostProps {
     title: string;
     content: string;
     updatedAt: string;
-  }
+  };
 }
 
 const Post = ({ post }: PostProps) => {
-  console.log('pppost', post)
   return (
     <>
       <Head>
@@ -33,38 +32,50 @@ const Post = ({ post }: PostProps) => {
         </article>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
 
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession({ req })
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
   const { slug } = params;
 
-  // if (!session) {
+  console.log("@@@ PASSOU PELO GETSERVERSIDEPROPS");
 
-  // }
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
-  const prismic = getPrismicClient(req)
+  const prismic = getPrismicClient(req);
 
-  const response = await prismic.getByUID<any>('post', String(slug), {})
+  const response = await prismic.getByUID<any>("post", String(slug), {});
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
-    updatedAt: new Date(response.last_publication_date).toLocaleString('pt-BR', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric',
-    })
-  }
+    updatedAt: new Date(response.last_publication_date).toLocaleString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
 
   return {
     props: {
       post,
-    }
-  }
-
-}
+    },
+  };
+};
