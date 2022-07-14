344ad5573183da0bfe37949475794797343cd47c
Thiago <thiagocdn@gmail.com>
2022-07-13 21:19:07 -0300

    Created preview page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet" />
 
-          <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+          {/* <link rel="shortcut icon" href="favicon.png" type="image/x-icon" /> */}
         </Head>
         <body>
           <Main />
